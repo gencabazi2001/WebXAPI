49edc9527d60a9c17f7c0440a9adcfb069c42f62
Buqeta <73654883+Buqeta@users.noreply.github.com>
2021-07-19 00:38:36 +0200

    Contact Us, Ankesat (Buqeta)

--- qaip-react-app/src/App.js
+++ qaip-react-app/src/App.js
@@ -21,7 +21,7 @@ import Dashboard from "./pages/Dashboard";
 import Group from './PostFeed/Group'
 import Home from './pages/index';
 import AdmNavbar from '../src/components/NavBar/NavbarAdm'
-
+import ContactUs from './pages/ContactUs'
 
 
 const App = (props) => {
@@ -48,7 +48,8 @@ const App = (props) => {
         <ProtectedRoute path="/groupmembers" exact component={GroupMembers} navbar={NavBar}  />
         <ProtectedRoute path="/departments" exact component={Departments} navbar={NavBar}  />
         <ProtectedRoute path="/dashboard" exact component={Dashboard} navbar={AdmNavbar}  />
-        <Route path="/post"exact>
+        <ProtectedRoute path="/contactUs" exact component={ContactUs} navbar={NavBar}  />
+          <Route path="/post"exact>
           <Post/>
         </Route>
         {/* <Route path ="/dashboard" exact>
